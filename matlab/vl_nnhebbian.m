4d8cf7807b468f0bfee8a3ba0088546b076cfa6b
djsaunde <djsaunde@umass.edu>
2017-07-06 13:35:56 -0400

    More work on Hebbian learning layer: sparse matrix, correct vectorization (?).

--- matlab/vl_nnhebbian.m
+++ matlab/vl_nnhebbian.m
@@ -30,28 +30,38 @@ if strcmp(opts.mode, 'train')
     if isempty(dzdy)
         y = x .* (ones(size(x)) + opts.lambda * v * x) ;
     else
+        x = sparse(double(x)) ;
         y = dzdy .* (ones(size(x)) + opts.lambda * (v * x + x .* sum(v, 2))) ;
+        x = single(full(x)) ;
         
-        old_v = v ;
-        v = old_v + opts.eta * (x * x') .* double(old_v ~= 0) ;
+        old_v = full(v) ;
+        v = old_v + opts.eta * mean((reshape(x,[1, size(x, 1), size(x, 2)]) ...
+          .* reshape(x, [size(x, 1), 1, size(x, 2)])) .* double(old_v ~= 0), 3) ;
         v(v > 1) = 1 ; v(v < -1) = -1 ;
+        v = sparse(double(v)) ;
         
-        figure(2) ;
-        clf ;
-        histogram(v(v ~= 0), linspace(-1, 1, 150), 'FaceColor', 'm') ;
-        xlim([-1, 1]) ;
-        ylim([0, 50]) ;
+%         x = single(full(x)) ;
+%         y = single(full(y)) ;
         
-        % xlim([weight_counter - 15, weight_counter]) ;
-        % ylim([-1, 1]) ;
-        % plot(ones(numel(v), 1) * weight_counter, v(:), '.b', 'MarkerSize', 1) ;
-        
-        pause(0.01) ;
-        hold on ;
+%         figure(2) ;
+%         clf ;
+%         histogram(v(v ~= 0), linspace(-1, 1, 150), 'FaceColor', 'm') ;
+%         xlim([-1, 1]) ;
+%         ylim([0, 5000]) ;
+%         
+%         % xlim([weight_counter - 15, weight_counter]) ;
+%         % ylim([-1, 1]) ;
+%         % plot(ones(numel(v), 1) * weight_counter, v(:), '.b', 'MarkerSize', 1) ;
+%         
+%         pause(0.01) ;
+%         hold on ;
     end
 else
     if nargin <= 1 || isempty(dzdy)
+        x = sparse(double(x)) ;
         y = x .* (ones(size(x)) + opts.lambda * v * x) ;
+        x = single(full(x)) ;
+        y = single(full(y)) ;
     else
         y = dzdy .* (ones(size(x)) + opts.lambda * (v * x + x .* sum(v, 2))) ;
     end
